39b9c7ee44cb697fb81ae43d9a4d751e475988ad
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-03-04 22:55:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,15 @@
 require 'pry'
 
 class Artist
+ 
   attr_accessor :name
   attr_reader :songs
+  
 
+  extend Memorable
+  include Findable::ClassMethods
+  
+  
   @@artists = []
 
   def initialize
@@ -11,20 +17,9 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
 
   def self.all
     @@artists
-  end
-
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    self.all.count
   end
 
   def add_song(song)
